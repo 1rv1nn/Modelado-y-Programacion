762d7b5e44a20cdda6771b0a05f5d2d465a80723
fwgalde <f.ugalde@ciencias.unam.mx>
2022-09-08 17:20:42 -0500

    Modificaciones a la clase espectador

--- Practica01/src/Espectador.java
+++ Practica01/src/Espectador.java
@@ -2,18 +2,28 @@ public class Espectador implements Observador {
     String idViewer;
     String peleadorFavorito;
     String resumen;
+    String username;
     CampoDeBatalla stream;
     
-    public Espectador(String idViewer, String peleadorFavorito) {
-	this.idViewer = idViewer;
-	this.peleadorFavorito = peleadorFavorito;
+    public Espectador(String idViewer, String peleadorFavorito, String username) {
+    	this.idViewer = idViewer;
+    	this.peleadorFavorito = peleadorFavorito;
+    	this.username = username;
+    	stream = new CampoDeBatalla();
     }
 
     public void actualizar() {
-	resumen = stream.getMensaje();
+    	resumen = stream.getMensaje();
     }
 
     public void desconectarse() {
-	stream.remover();
+    	stream.remover(this);
+    }
+    
+    public void verDirecto() {
+    	System.out.println("Username: " + username
+    			+ "\nID: " + idViewer
+    			+ "\nPeleador favorito: " + peleadorFavorito 
+    			+ "\n Lo que ha pasado es: " + resumen);
     }
 }
