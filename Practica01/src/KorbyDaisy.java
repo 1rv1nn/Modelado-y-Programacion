ce84a1ca85489eca84622f5b34ce72525b2faa5b
fwgalde <f.ugalde@ciencias.unam.mx>
2022-09-08 04:36:09 -0500

    Korby absorbiendo a Daisy

---
+++ Practica01/src/KorbyDaisy.java
@@ -0,0 +1,8 @@
+
+public class KorbyDaisy extends Korby {
+	public KorbyDaisy(int vida) {
+	    this.vida = vida;
+	    ataque = new AtaqueLanzandoAToad();
+	    defensa = new DefensaEscudoDeToad();
+	}
+}
