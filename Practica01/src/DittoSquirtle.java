5b9a9a7a69671f3726a492ffb7fa61918990bfc8
fwgalde <f.ugalde@ciencias.unam.mx>
2022-09-08 17:19:48 -0500

    Ditto cuando absorbe al escuirol

---
+++ Practica01/src/DittoSquirtle.java
@@ -0,0 +1,8 @@
+
+public class DittoSquirtle extends Ditto {
+	public DittoSquirtle(int vida) {
+	    this.vida = vida;
+	    ataque = new AtaqueEscupeAgua();
+	    defensa = new DefensaDeAgua();
+	}
+}
