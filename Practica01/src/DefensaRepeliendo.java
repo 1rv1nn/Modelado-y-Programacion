bbcf2b4d4659bd093692a618ed2b836d0872a3eb
fwgalde <f.ugalde@ciencias.unam.mx>
2022-09-08 04:19:35 -0500

    Cambiamos el valor de la defensa

--- Practica01/src/DefensaRepeliendo.java
+++ Practica01/src/DefensaRepeliendo.java
@@ -3,6 +3,6 @@ public class DefensaRepeliendo implements IDefensaMeganman {
 
 	@Override
 	public void defiende(Meganman m){
-	    m.setVida(m.getVida() + 5);
+	    m.setVida(m.getVida() + 8);
 	}
 }
