5c599318ea93060eefb6d013865b83d61405e1a2
fwgalde <f.ugalde@ciencias.unam.mx>
2022-09-08 04:23:52 -0500

    Cambio de valores de sanación

--- Practica01/src/DefensaBarril.java
+++ Practica01/src/DefensaBarril.java
@@ -3,7 +3,7 @@ public class DefensaBarril implements IDefensaKorby {
 
 	@Override
 	public void defiende(Korby kirby) {
-	    kirby.setVida(kirby.getVida() + 15);		
+	    kirby.setVida(kirby.getVida() + 9);		
 	}
 
 }
--- Practica01/src/DefensaEscudoDeToad.java
+++ Practica01/src/DefensaEscudoDeToad.java
@@ -3,7 +3,7 @@ public class DefensaEscudoDeToad implements IDefensaKorby {
 
 	@Override
 	public void defiende(Korby kirby) {
-	    kirby.setVida(kirby.getVida() + 20);		
+	    kirby.setVida(kirby.getVida() + 12);		
 	}
 	
 }
