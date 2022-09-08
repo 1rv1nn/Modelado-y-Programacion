34ca6e879ab4e8008745e72b26a5d10dddd780e4
fwgalde <f.ugalde@ciencias.unam.mx>
2022-09-08 04:08:40 -0500

    Comentamos que es un método sobreescrito

--- Practica01/src/AtaqueSimpleMega.java
+++ Practica01/src/AtaqueSimpleMega.java
@@ -1,7 +1,9 @@
 
 
 public class AtaqueSimpleMega implements IAtaqueMeganman {
-    public void ataca(Korby kirby, Ditto dittu){
+    
+	@Override
+	public void ataca(Korby kirby, Ditto dittu){
 	kirby.setVida(kirby.getVida() - 10);
 	dittu.setVida(dittu.getVida() - 10);
     }
