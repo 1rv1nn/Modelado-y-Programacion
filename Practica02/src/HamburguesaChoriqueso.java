1052ff64533bc4b0acf0bd93fbf35312cf9433fe
fwgalde <f.ugalde@ciencias.unam.mx>
2022-09-20 04:40:23 -0500

    Agregamos las hamburguesas de choriqueso

---
+++ Practica02/src/HamburguesaChoriqueso.java
@@ -0,0 +1,22 @@
+/**
+ * Clase que implementa la preparación de una Hamburguesa de Choriqueso.
+ * Además extiende de Hamburguesa.
+ * 
+ * @author Cruz González, Irvin Javier
+ * @author Ugalde Flores, Jimena
+ * @author Ugalde Ubaldo, Fernando
+ * @version 1.0
+ * @since Semester: 2023-1
+ */
+public class HamburguesaChoriqueso extends Hamburguesa{
+
+    @Override
+    void prepararCarne() {
+        System.out.println("Con carne de res, huevo, pan molido y chorizo hacemos las hamburguesas.");
+    }
+
+    @Override
+    void ponerQueso() {
+        System.out.println("Agregamos queso asadero.");
+    }
+}
