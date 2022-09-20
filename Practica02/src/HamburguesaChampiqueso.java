b5ff96a42644d3f0ffac451838892e59a12c1156
Jimena Ugalde Flores <jimenaugalde@ciencias.unam.mx>
2022-09-20 11:04:22 -0700

    template

---
+++ Practica02/src/HamburguesaChampiqueso.java
@@ -0,0 +1,36 @@
+/**
+ * Clase que implementa la preparación de una Hamburguesa de Choriqueso.
+ * Además extiende de Hamburguesa.
+ * 
+ * @author Cruz González, Irvin Javier
+ * @author Ugalde Flores, Jimena
+ * @author Ugalde Ubaldo, Fernando
+ * @version 1.0
+ * @since Semester: 2023-1
+ */
+public class HamburguesaChampiqueso extends Hamburguesa {
+
+    public HamburguesaChampiqueso() {
+        id = 6;
+        nombre = "Hamburguesa Champiqueso.";
+        precio = 36;
+        tieneQueso = true;
+        esVegetariano = false;
+        descripcion = "ID: " + id
+                    + "\nNombre: " + nombre
+                    + "\nPrecio: " + precio
+                    + "\nTiene queso"
+                    + "\nNo es vegetariana"
+                    + "Deliciosa hamburguesa de champiqueso.";
+    }
+
+    @Override
+    void prepararCarne() {
+        System.out.println("Con carne molida, huevo, champiñones rebanados y cebolla picada hacemos las hamburguesas.");
+    }
+
+    @Override
+    void ponerQueso() {
+        System.out.println("Agregamos queso gouda.");
+    }
+}
---
+++ Practica02/src/HamburguesaRanchera.java
@@ -0,0 +1,37 @@
+/**
+ * Clase que implementa la preparación de una Hamburguesa Ranchera.
+ * Además extiende de Hamburguesa.
+ * 
+ * @author Cruz González, Irvin Javier
+ * @author Ugalde Flores, Jimena
+ * @author Ugalde Ubaldo, Fernando
+ * @version 1.0
+ * @since Semester: 2023-1
+ */
+public class HamburguesaRanchera extends Hamburguesa {
+
+    public HamburguesaRanchera (){
+
+        id = 5;
+        nombre = "Hamburgesa Ranchera.";
+        precio = 46;
+        tieneQueso = false;
+        esVegetariano = false;
+        descripcion = "ID: " + id
+                    + "\nNombre: " + nombre
+                    + "\nPrecio: " + precio
+                    + "\nNo tiene queso"
+                    + "\nNo es vegetariana"
+                    + "Deliciosa hamburguesa ranchera.";
+    }
+
+    @Override
+    void prepararCarne() {
+        System.out.println("Con carne de res molida, unas rebanadas de tocino, chorizo de res, y salsa BBQ hacemos las hamburguesas.");
+    }
+
+    @Override
+    void ponerQueso() {
+        System.out.println("No tiene queso.");
+    }
+}
