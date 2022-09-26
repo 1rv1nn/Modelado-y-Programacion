661b007447010897756b30ea8b6fe6873a32e740
fwgalde <f.ugalde@ciencias.unam.mx>
2022-09-26 04:00:35 -0500

    Agregamos la pizza Hawaiana

---
+++ Practica03/src/PizzaHawaina.java
@@ -0,0 +1,33 @@
+/**
+ * Clase que implementa la pizza hawaiana.
+ */
+public class PizzaHawaina implements Pizza{
+
+    private final String nombre = "pizza hawaiana"; 
+
+    @Override
+    public String getNombre(){
+        return nombre;
+    }
+
+    @Override
+    public String getQueso() {
+        return "Queso Cheddar \t\t\t$50";
+    }
+
+    @Override
+    public String getCarne() {
+        return "Jamón \t\t\t\t$25";
+    }
+
+    @Override
+    public String getMasa() {
+        return "Masa delgada \t\t\t$75";
+    }
+
+    @Override
+    public double precio() {
+        return 150;
+    }
+    
+}
