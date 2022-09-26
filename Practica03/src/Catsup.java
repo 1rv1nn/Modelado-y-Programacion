5b5ce71ffbc6ef674101bc9449dd8d7c893d8b2c
1rv1nn <1rv1n@ciencias.unam.mx>
2022-09-26 01:28:23 -0500

    Clases

---
+++ Practica03/src/Catsup.java
@@ -0,0 +1,31 @@
+/**
+ * Clase que decora al Alimento e implementa la interface Ingrediente. 
+ * 
+ * @author Cruz González Irvin Javier
+ * @author Ugalde Flores Jimena
+ * @author Ugalde Ubaldo Fernando
+ * 
+ * @version 1.0
+ * 
+ */
+public class Catsup extends Ingrediente{
+
+    /**
+     * Método contructor
+     * @param alimento El alimento al cual se le agregará la cebolla
+     */
+    public Catsup(Alimento alimento){
+        this.alimento=alimento;
+    }
+
+    @Override
+    public String getDescripcion(){
+        return alimento.getDescripcion() + "\nCon catsup \t$0";
+    }
+
+    @Override
+    public double precio(){
+        return alimento.precio() + 0;
+    }
+}
+
