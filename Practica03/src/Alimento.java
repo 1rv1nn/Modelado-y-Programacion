68b5c8aa4a5d4e31d46fe9931f2ee08adc37a6b9
fwgalde <f.ugalde@ciencias.unam.mx>
2022-09-25 21:18:26 -0500

    Agregamos documentación

--- Practica03/src/Alimento.java
+++ Practica03/src/Alimento.java
@@ -1,4 +1,18 @@
+/**
+ * Interface de alimento que contiene los métodos de cada alimento.
+ */
 public interface Alimento {
-    void getDescripcion();
-    int precio();
+    /**
+     * Método para obtener la descripción del alimento.
+     * 
+     * @return Información de los ingredientes (con su precio) del alimento seleccionado. 
+     */
+    String getDescripcion();
+
+    /**
+     * Método para obtener el precio del alimento.
+     * 
+     * @return Precio del alimento.
+     */
+    double precio();
 }
