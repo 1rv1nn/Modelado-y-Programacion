189948f5f617f14ab8dd9cf4a82f4fc1aa5e73ed
fwgalde <f.ugalde@ciencias.unam.mx>
2022-09-26 23:37:55 -0500

    Documentacion

--- Practica03/src/Test.java
+++ Practica03/src/Test.java
@@ -1,3 +1,13 @@
+/**
+ * Clase que contiene el método main.
+ * 
+ * @author Cruz González, Irvin Javier
+ * @author Ugalde Flores, Jimena
+ * @author Ugalde Ubaldo, Fernando
+ * 
+ * @version 1.0
+ * 
+ */
 public class Test {
     public static void main(String[] args) {
         MenuWaySub.imprimeMenuPrincipal();
